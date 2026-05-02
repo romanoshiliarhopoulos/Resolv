b7d9aea16a0a0ddd07d1c708c1154b074090494f
Romanos Chiliarchopoulos <romanoshiliarhopoulos@gmail.com>
2026-05-02 13:00:31 -0500

    new

--- src/model/unet.py
+++ src/model/unet.py
@@ -1,47 +1,55 @@
 import torch
 import torch.nn as nn
+from torch.utils.checkpoint import checkpoint
+
 from .unet_parts import DoubleConv, Down, Up, OutConv
 
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, out_channels, bilinear=False):
-        super(UNet, self).__init__()
-        self.n_channels = n_channels
+    """Blind residual denoising U-Net. Predicts noise and subtracts it from the input."""
+
+    def __init__(
+        self,
+        in_channels: int = 3,
+        out_channels: int = 3,
+        base_channels: int = 64,
+        bilinear: bool = False,
+    ):
+        super().__init__()
+        self.in_channels = in_channels
         self.out_channels = out_channels
-        self.bilinear = bilinear
+        self._checkpointing = False
 
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
-        self.down3 = Down(256, 512)
+        c = base_channels
         factor = 2 if bilinear else 1
-        self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, out_channels)
 
-    def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        return self.outc(x)
+        self.inc = DoubleConv(in_channels, c)
+        self.down1 = Down(c, 2 * c)
+        self.down2 = Down(2 * c, 4 * c)
+        self.down3 = Down(4 * c, 8 * c)
+        self.down4 = Down(8 * c, 16 * c // factor)
+        self.up1 = Up(16 * c, 8 * c // factor, bilinear)
+        self.up2 = Up(8 * c, 4 * c // factor, bilinear)
+        self.up3 = Up(4 * c, 2 * c // factor, bilinear)
+        self.up4 = Up(2 * c, c, bilinear)
+        self.outc = OutConv(c, out_channels)
+
+    def _run(self, module, *args):
+        if self._checkpointing and self.training:
+            return checkpoint(module, *args, use_reentrant=False)
+        return module(*args)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x1 = self._run(self.inc, x)
+        x2 = self._run(self.down1, x1)
+        x3 = self._run(self.down2, x2)
+        x4 = self._run(self.down3, x3)
+        x5 = self._run(self.down4, x4)
+        y = self._run(self.up1, x5, x4)
+        y = self._run(self.up2, y, x3)
+        y = self._run(self.up3, y, x2)
+        y = self._run(self.up4, y, x1)
+        return (x - self.outc(y)).clamp(0.0, 1.0)
 
     def use_checkpointing(self):
-        self.inc = torch.utils.checkpoint.checkpoint_wrapper(self.inc)
-        self.down1 = torch.utils.checkpoint.checkpoint_wrapper(self.down1)
-        self.down2 = torch.utils.checkpoint.checkpoint_wrapper(self.down2)
-        self.down3 = torch.utils.checkpoint.checkpoint_wrapper(self.down3)
-        self.down4 = torch.utils.checkpoint.checkpoint_wrapper(self.down4)
-        self.up1 = torch.utils.checkpoint.checkpoint_wrapper(self.up1)
-        self.up2 = torch.utils.checkpoint.checkpoint_wrapper(self.up2)
-        self.up3 = torch.utils.checkpoint.checkpoint_wrapper(self.up3)
-        self.up4 = torch.utils.checkpoint.checkpoint_wrapper(self.up4)
-        self.outc = torch.utils.checkpoint.checkpoint_wrapper(self.outc)
+        self._checkpointing = True
